8d563d239dc0e4e4c2892617bcb093521db350f5
Jameson Gamble <jamesongamble@gmail.com>
2015-11-25 12:07:08 -0800

    Sets up default route to main

--- app/templates/client/app.js
+++ app/templates/client/app.js
@@ -1,7 +1,15 @@
 'use strict';
 
 angular.module('<%= scriptAppName %>', [<%- angularModules %>])
-  .config(function(<% if (filters.ngroute) { %>$routeProvider<% } if (filters.uirouter) { %>$urlRouterProvider<% } %>, $locationProvider) {<% if (filters.ngroute) { %>
+  .config(function(<% if (filters.ngroute) { %>$routeProvider<% } if (filters.uirouter) { %>$urlRouterProvider,$stateProvider<% } %>, $locationProvider) {<% if (filters.ngroute) { %>
+    
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'templates/main.html',
+        controller: 'MainCtrl'
+      });
+
     $routeProvider
       .otherwise({
         redirectTo: '/'
